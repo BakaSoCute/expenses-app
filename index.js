ccd2e68184d124d930998fb0c62ba02204c908fb
Aleksandr Omelucik <bladewizard@mail.ru>
2025-04-06 11:38:14 +0300

    complete expenses app v1.0

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const expenses = [];
+const LIMIT = 10000;
+const CURRENCY = " руб.";
+const STATUS_IN_LIMIT = "ВСЁ ХОРОШО";
+const STATUS_OUT_OF_LIMIT = "ВСЁ ПЛОХО";
+const STATUS_OUT_OF_LIMIT_CLASSNAME = "status_red";
+
 const inputNode = document.getElementById("input");
 const buttonNode = document.getElementById("butt");
 const historyNode = document.getElementById("history")
@@ -6,37 +11,73 @@ const sumNode = document.getElementById("sum");
 const limitNode = document.getElementById("limit");
 const statusNode = document.getElementById("status");
 
-const LIMIT = 10000;
+const expenses = [];
+
+init(expenses);
+
 
 
 buttonNode.addEventListener("click", function () {
 
-    if (inputNode.value === "") {
-        return;
-    }
+   const expense = getExpanceFromUser();
 
-    const expense = parseInt(inputNode.value);
-    inputNode.value = "";
+   if (!expense) {
+    return;
+   }
+   trackExpance(expense);
 
+    render(expenses)
+
+});
+
+function init(expenses) {
+    limitNode.innerText = LIMIT;
+    statusNode.innerText = STATUS_IN_LIMIT;
+    sumNode.innerText = calculateExpanses(expenses);
+}
+function trackExpance(expense) {
     expenses.push(expense);
-    let expensesListHTML = "";
-
-    expenses.forEach(element =>{
-        expensesListHTML += `<li>${element} руб.</li>`
-    });
-    historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
-    
+}
+function getExpanceFromUser() {
+    if (inputNode.value === "") {
+        return null;
+    };
+    const expense = parseInt(inputNode.value);
+    clearInput();
+    return expense;
+}
+function clearInput() {
+    inputNode.value = "";
+}
+function calculateExpanses(expenses) {
     let sum = 0;
     expenses.forEach(element =>{
         sum += element;
     });
+    return sum;
+}
+function render(expenses) {
+    const sum = calculateExpanses(expenses);
+    renderHistory(expenses);
+    renderSum(sum);
+    renderStatus(sum);
+}
+function renderHistory(expenses) {
+    let expensesListHTML = "";
+
+    expenses.forEach(element =>{
+        expensesListHTML += `<li>${element},${CURRENCY}</li>`;
+    });
+    historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+};
+function renderSum(sum) {
     sumNode.innerText = sum;
-
+}
+function renderStatus(sum) {
     if (sum <=  LIMIT) {
-        statusNode.innerText = "всё хорошо";
+        statusNode.innerText = STATUS_IN_LIMIT;
     } else {
-        statusNode.innerText = "всё плохо";
-        statusNode.classList.add("status_red");
+        statusNode.innerText = STATUS_OUT_OF_LIMIT;
+        statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
     }
-
-})
+}
