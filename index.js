fe6ad5b9afefdd966e4c8d532bb2dd29a459e6d6
Aleksandr Omelucik <bladewizard@mail.ru>
2025-04-06 10:10:53 +0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const expenses = [];
 const inputNode = document.getElementById("input");
 const buttonNode = document.getElementById("butt");
+const historyNode = document.getElementById("history")
+const sumNode = document.getElementById("sum");
+const limitNode = document.getElementById("limit");
+const statusNode = document.getElementById("status");
+
+const LIMIT = 10000;
+
 
 buttonNode.addEventListener("click", function () {
 
-    const expense = inputNode.value;
+    if (inputNode.value === "") {
+        return;
+    }
+
+    const expense = parseInt(inputNode.value);
+    inputNode.value = "";
+
     expenses.push(expense);
-    console.log(expenses);
+    let expensesListHTML = "";
+
+    expenses.forEach(element =>{
+        expensesListHTML += `<li>${element} руб.</li>`
+    });
+    historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+    
+    let sum = 0;
+    expenses.forEach(element =>{
+        sum += element;
+    });
+    sumNode.innerText = sum;
+
+    if (sum <=  LIMIT) {
+        statusNode.innerText = "всё хорошо";
+    } else {
+        statusNode.innerText = "всё плохо";
+        statusNode.classList.add("status_red");
+    }
+
 })
